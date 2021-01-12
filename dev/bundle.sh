987b9f1f4a76e0d5bb86f8471f53f7ceccca22d9
dbolharovcmx <74190431+dbolharovcmx@users.noreply.github.com>
2021-01-12 10:34:28 +0200

    Update dev/bundle.sh
    Co-authored-by: maor-rosenfeld <49363375+maor-rosenfeld@users.noreply.github.com>

--- dev/bundle.sh
+++ dev/bundle.sh
@@ -2,7 +2,7 @@
 
 echo ${VERSION-Unknown} > VERSION
 
-sed -i "s/SCA_AGENT_VERSION: \${AGENT_VERSION}/SCA_AGENT_VERSION: ${VERSION-Unknown}/g" docker-compose.yml
+sed -i "s/\${AGENT_VERSION}/${VERSION-Unknown}/g" docker-compose.yml
 
 docker run --rm -v "${PWD}":/sca-agent -w /sca-agent aweponken/alpine-zip zip -r "${1-sca-agent.local.zip}" \
   README.md \
