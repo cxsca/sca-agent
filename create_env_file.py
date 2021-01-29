35cbf2774f7ae7e5a6dcb1f6993a90d54a5a5963
dmytro.bolharov <dmytro.bolharov@checkmarx.com>
2021-01-29 15:13:05 +0200

    Refactorings python naming convention

--- create_env_file.py
+++ create_env_file.py
@@ -13,10 +13,10 @@ placeholder = "%AUTO_GENERATED_PASSWORD%"
 input_file = open(".env.defaults")
 output_file = open(".env", 'w')
 
-for envLine in input_file.readlines():
-    for match_position in [match.start() for match in re.finditer(re.escape(placeholder), envLine)]:
-        envLine = envLine.replace(placeholder, get_random_hex_string(), 1)
-    output_file.write(envLine)
+for env_line in input_file.readlines():
+    for match_position in [match.start() for match in re.finditer(re.escape(placeholder), env_line)]:
+        env_line = env_line.replace(placeholder, get_random_hex_string(), 1)
+    output_file.write(env_line)
 
 output_file.close()
 input_file.close()
