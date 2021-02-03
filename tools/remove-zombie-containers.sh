4c6d7e56dcc2458f5112a1a33e5e2aa877800853
SnirCm <52414923+SnirCm@users.noreply.github.com>
2021-02-03 18:18:26 +0200

    Add script to kill zombie containers (#8)
    * killing zombie containers that was super hard
    
    * fix comment
    
    * add help and silence mode
    
    * exit if no zombies
    
    * run with bash
    
    * fix some things
    
    * ?
    
    * fix
    
    * github is doing problems
    
    * fix his problems
    
    * replace messages

---
+++ tools/remove-zombie-containers.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+SILENCE=0
+for arg in "$@"
+do
+    case $arg in
+    -h|--help)
+        echo "Usage: show-scans-status [options]"
+        echo "options:"
+        echo "-h, --help               show this help message"
+        echo "-s, --silence            don't wait for approval to remove containers"
+        echo "examples:"
+        echo "Linux and MinGw distribution (gitbash, cygwin etc.)"
+        echo "  $ sh show-scans-status.sh"
+        echo "WSL2 ubuntu:"
+        echo "  $ /bin/bash show-scans-status.sh"
+        exit 0
+        ;;
+    -s|--silence)
+        SILENCE=1
+        shift 
+        ;;
+    esac
+done
+
+ids=$(docker ps --no-trunc -f name=sca-agent* --format "{{.ID}}\n")
+c=$(echo -n "$ids" | wc -l)
+if [ $c -eq 0 ]; then
+ echo "No zombie containers to delete"
+ exit 0
+fi
+
+echo "Removing following containers:"
+docker ps --no-trunc -f name=sca-agent* --format "table {{.ID}}\t{{.Names}}" || exit
+echo "-------------------------------------------"
+
+if [ $SILENCE -eq 0 ]; then
+   echo "Press any key to continue..."
+   read -n 1
+fi
+
+echo "Starting"
+
+# check if Linux or wsl/mingw by try find cmd.exe
+cmd=$(command -v cmd.exe || echo shell)
+if [ "$cmd" != 'shell' ]; then
+   echo "Creating zombie container with cmd to access MobyLinuxVM"
+   cmd.exe /C "docker run --rm --name zombie --net=host --ipc=host --uts=host --pid=host --security-opt=seccomp=unconfined --privileged -it -d -v /:/host alpine sh" || exit
+   removeCmd='cmd.exe /C "docker exec -i zombie rm -r /host/var/lib/docker/containers"'
+else
+   removeCmd='rm -r /var/lib/docker/containers'
+fi
+echo "-------------------------------------------"
+for id in $(echo "$ids")
+do
+   if [ "$cmd" = 'shell' ]; then
+      umount /var/lib/docker/containers/$id/mounts/shm&> /dev/null && echo "$id container unmounted" || echo "$id nothing to unmount"
+   fi
+   eval "$removeCmd/$id" && echo "$id container deleted" || continue
+done 
+
+echo "-------------------------------------------"
+if [ "$cmd" != 'shell' ]; then
+   zombie=$(docker container rm -f zombie)
+   if [ "$zombie" = 'zombie' ]; then
+      echo "Zombie container deleted"
+   else
+      echo "Error deleting zombie container"
+      exit
+   fi
+   echo -e "\e[1mPlease restart docker to complete the operation"
+else
+   echo "Restarting docker"
+   service docker restart
+fi
