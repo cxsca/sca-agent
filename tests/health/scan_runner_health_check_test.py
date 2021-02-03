05ef040b221ace0394e823b1a83957e6c9c7ba84
dmytro.bolharov <dmytro.bolharov@checkmarx.com>
2021-02-03 14:44:50 +0200

    Naming convention refactorings

--- tests/health/scan_runner_health_check_test.py
+++ tests/health/scan_runner_health_check_test.py
@@ -1,14 +1,14 @@
 from tests.base.tests_base import TestsBase
 
 
-class Scan_Runner_HealthCheck_Test(TestsBase):
+class ScanRunnerHealthcheckTest(TestsBase):
 
     def __init__(self):
         super().__init__(test_name="ScanRunnerHealthCheck")
 
     def run_test(self):
         # Run the agent
-        super(Scan_Runner_HealthCheck_Test, self).agent_run()
+        super(ScanRunnerHealthcheckTest, self).agent_run()
 
         # Stop the agent
-        super(Scan_Runner_HealthCheck_Test, self).agent_stop()
+        super(ScanRunnerHealthcheckTest, self).agent_stop()
