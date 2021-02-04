cd133bd796b215aacc25dec535b7261b8fca23f9
maor-rosenfeld <49363375+maor-rosenfeld@users.noreply.github.com>
2021-02-04 11:55:51 +0100

    Fix and verify bundle script (#12)

--- dev/bundle.sh
+++ dev/bundle.sh
@@ -5,12 +5,12 @@ echo ${VERSION-Unknown} > VERSION
 sed -i "s/\${AGENT_VERSION}/${VERSION-Unknown}/g" docker-compose.yml
 
 docker run --rm -v "${PWD}":/sca-agent -w /sca-agent aweponken/alpine-zip zip -r "${1-sca-agent.local.zip}" \
-  README.md \
-  setup.sh \
-  show-scans-status.sh \
+  logs \
+  tools \
+  volumes \
   .env.defaults \
   docker-compose.yml \
-  volumes \
-  logs \
-  VERSION \
-  LICENSE
+  LICENSE \
+  README.md \
+  setup.sh \
+  VERSION
