dc769473ade3ef47d802ffdf4edb451d817bda98
dmytro.bolharov <dmytro.bolharov@checkmarx.com>
2021-02-11 14:35:36 +0200

    Test failure inside the test

--- tests/openid/src/scripts/run_tests.py
+++ tests/openid/src/scripts/run_tests.py
@@ -1,3 +1,11 @@
 # Not finished, will be another PR to finish the tests logic
+import sys
 
-print("OpenID Logic")
+
+class AgentErrror(Exception):
+    def __init__(self):
+        super("Error during processing the Agent Test")
+
+
+# print("OpenID Logic")
+raise AgentErrror
