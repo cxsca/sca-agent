bafe2efb0df4845ae75e42b487be2ebaa0979fd6
dmytro.bolharov <dmytro.bolharov@checkmarx.com>
2021-02-16 19:28:31 +0200

    Add minio health check

--- tests/customport/src/scripts/customport_tests.py
+++ tests/customport/src/scripts/customport_tests.py
@@ -7,13 +7,23 @@ class CustomPortTests(unittest.TestCase):
     def __init__(self, f):
         super(CustomPortTests, self).__init__(f)
 
-        self.route_to_test = "http://localhost:5001/api/health"
+        self.localhost = "http://localhost:5001"
 
-    def test_status_code(self):
-        response = requests.get(self.route_to_test)
+    def test_scan_runner_healthy(self):
+        response = requests.get(f"{self.localhost}/api/health")
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()["status"], "available")
+
+    def test_minio_with_custom_port_healthy(self):
+
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, '
+                                 'like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+        response = requests.get(f"{self.localhost}/minio/login", headers=headers)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.content)
 
 
 # Make container properly exit, when tests fail
