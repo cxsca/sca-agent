a2dbb510e0ae4d773a8d9f3a8fcd554b999e781c
dmytro.bolharov <dmytro.bolharov@checkmarx.com>
2021-03-12 22:08:11 +0200

    Skip tests while repos are not public

--- tests/src/scan/scripts/scan_goat_tests.py
+++ tests/src/scan/scripts/scan_goat_tests.py
@@ -1,19 +1,22 @@
 from test_base import ScanBase
-
+import unittest
 
 class GoatScanTests(ScanBase):
     __test__ = True
 
+    @unittest.skip("Repository is not public yet")
     def test_goat_scan_small(self):
         self.create_project("SCAAgent-Small-Goat")
         scan_success = self.start_scan_and_wait(type="git", url="https://github.com/cxsca/sca-small-goat")
         self.assertTrue(scan_success)
 
+    @unittest.skip("Repository is not public yet")
     def test_goat_scan(self):
         self.create_project("SCAAgent-Goat")
-        scan_success = self.start_scan_and_wait(type="git", url="https://github.com/sca-small-goat", num_of_checks=30)
+        scan_success = self.start_scan_and_wait(type="git", url="https://github.com/sca-goat", num_of_checks=30)
         self.assertTrue(scan_success)
 
+    @unittest.skip("Repository is not public yet")
     def test_goat_scan_big(self):
         self.create_project("SCAAgent-Big-Goat")
         scan_success = self.start_scan_and_wait(type="git", url="https://github.com/sca-big-goat", num_of_checks=60)
