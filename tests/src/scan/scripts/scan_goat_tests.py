20bababbaa11aa9c54cd9abd2771808ca4e01f23
dmytro.bolharov <dmytro.bolharov@checkmarx.com>
2021-03-12 15:19:36 +0200

    Update Scan base and Tests logic

--- tests/src/scan/scripts/scan_goat_tests.py
+++ tests/src/scan/scripts/scan_goat_tests.py
@@ -1,21 +1,9 @@
 from test_base import ScanBase
-import os
 
 
 class GoatScanTests(ScanBase):
     __test__ = True
 
     def test_goat_scan_small(self):
-        exit_code = self.start_scan(project_name="ScaAgentSmallGoatScan",
-                                    location_directory=f"{os.getcwd()}/assets/small")
-        self.assertEqual(exit_code, 0)
-
-    def test_goat_scan(self):
-         exit_code = self.start_scan(project_name="ScaAgentGoatScan",
-                                     location_directory=f"{os.getcwd()}/assets/normal")
-         self.assertEqual(exit_code, 0)
-
-    def test_goat_scan_big(self):
-        exit_code = self.start_scan(project_name="ScaAgentBigGoatScan",
-                                    location_directory=f"{os.getcwd()}/assets/big")
-        self.assertEqual(exit_code, 0)
+        scan_success = self.start_scan_and_wait(type="git", url="https://github.com/cxsca-sca-agent")
+        self.assertTrue(scan_success)
--- tests/src/scan/scripts/test_base.py
+++ tests/src/scan/scripts/test_base.py
@@ -1,9 +1,14 @@
+import uuid
+import requests
 import unittest
 import os
+import time
 
 
 class ScanBase(unittest.TestCase):
     __test__ = False
+
+    await_time_in_secs = 15.0
 
     def __init__(self, f):
         super(ScanBase, self).__init__(f)
@@ -11,28 +16,87 @@ class ScanBase(unittest.TestCase):
         port = os.environ.get("AGENT_PORT") or "80"
         protocol = os.environ.get("AGENT_PROTOCOL") or "http"
 
-        self.CXAccount = os.environ.get("SCATENANT")
-        self.CXUser = os.environ.get("SCAUSERNAME")
-        self.CXPasswordSecret = os.environ.get("SCAPASSWORDSECRET")
-
         self.localhost = f"{protocol}://localhost:{port}"
 
-    def start_scan(self, project_name, url=None, location_directory=None):
+    def setUp(self):
+        self.authorize()
+        self.create_project("SCAAgentTest")
 
-        if not project_name:
-            raise Exception(f"Project name must not be empty")
+    def tearDown(self):
+        response = requests.delete(f"{self.localhost}/risk-management/projects/{self.project_id}",
+                                   headers={"Authorization": f"bearer {self.access_token}"}, verify=False)
+        self.assertEqual(response.status_code, 204)
 
-        location_argument = f"-locationurl {url}" if url else f"-scalocationpath {location_directory}" \
-            if location_directory else None
+    # Sign in
+    def authorize(self):
 
-        if not location_argument:
-            raise Exception(f"No source specified for the scan")
+        auth_body = {
+            "grant_type": "password",
+            "client_id": "sca_resource_owner",
+            "scope": "sca_api",
+            "acr_values": f"Tenant:{os.environ.get('TEST_SCA_TENANT')}",
+            "username": f"{os.environ.get('TEST_SCA_USERNAME')}",
+            "password": f"{os.environ.get('TEST_SCA_PASSWORD')}"
+        }
 
-        return os.system(f"CLI/runCxConsole.sh scascan "
-                         f"-scaapiurl {self.localhost} "
-                         f"-projectname {project_name} "
-                         f"-scaUsername {self.CXUser} "
-                         f"-scaPassword {self.CXPasswordSecret} "
-                         f"-scaAccount {self.CXAccount} "
-                         f"{location_argument}")
+        response = requests.post(f"{self.localhost}/identity/connect/token",
+                             headers={"Content-Type": "application/x-www-form-urlencoded"},
+                             data=auth_body, verify=False)
+
+        self.assertEqual(response.status_code, 200)
+        self.access_token = response.json()["access_token"]
+
+    def create_project(self, projectAlias):
+
+        project_body = {
+            "name": f"{projectAlias}-{uuid.uuid4().hex}"
+        }
+
+        response = requests.post(f"{self.localhost}/risk-management/projects",
+                             headers={"Authorization": f"bearer {self.access_token}"},
+                             json=project_body, verify=False)
+
+        self.assertEqual(response.status_code, 201)
+        self.project_id = response.json()["id"]
+        print(f"Project Id: `{self.project_id}`")
+
+    # Start Scan
+    def start_scan_and_wait(self, type, url, num_of_checks=20):
+
+        # Initiate a scan
+        request_body = {
+            "project": {
+                "id": f"{self.project_id}",
+                "type": f"{type}",
+                "handler": {
+                    "url": f"{url}"
+                }
+            }
+        }
+        response = requests.post(f"{self.localhost}/api/scans", headers={"Authorization": f"bearer {self.access_token}"}, json=request_body, verify=False)
+        self.assertEqual(response.status_code, 201)
+        scan_id = response.json()["id"]
+        print(f"Scan Id: `{scan_id}`")
+
+        for i in range(1, num_of_checks):
+
+            print(f"Waiting for the scan to finish (attempt #{i}) ...")
+            time.sleep(self.await_time_in_secs)
+
+            response = requests.get(f"{self.localhost}/risk-management/scans/{scan_id}/status", headers={"Authorization": f"bearer {self.access_token}"}, verify=False)
+            self.assertEqual(response.status_code, 200)
+            scan_status = response.json()["name"].lower()
+
+            if scan_status != "scanning":
+                print(f"Scan finished with status `{scan_status}`")
+                self.assertEqual(scan_status, "done")
+                return True
+
+        return False
+
+
+
+
+
+
 
