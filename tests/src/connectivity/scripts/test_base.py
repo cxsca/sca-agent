e30103ce5303561e35b39931942078d2b0ff26ca
dmytro.bolharov <dmytro.bolharov@checkmarx.com>
2021-03-25 13:37:16 +0200

    Add hmac signature generator

--- tests/src/connectivity/scripts/test_base.py
+++ tests/src/connectivity/scripts/test_base.py
@@ -1,5 +1,7 @@
+import hmac
 import os
 import unittest
+import hashlib
 
 
 class ConnectivityBase(unittest.TestCase):
@@ -11,3 +13,10 @@ class ConnectivityBase(unittest.TestCase):
         protocol = os.environ.get("AGENT_PROTOCOL") or "http"
 
         self.localhost = f"{protocol}://localhost:{port}"
+        self.salt = os.environ.get("WEBHOOK_SECRET")
+
+    def make_sha1(self, input):
+        return 'sha1=' + hmac.new(self.salt.encode('utf-8'), input.encode('utf-8'), hashlib.sha1).hexdigest()
+
+    def make_sha256(self, input):
+        return 'sha256=' + hmac.new(self.salt.encode('utf-8'), input.encode('utf-8'), hashlib.sha256).hexdigest()
